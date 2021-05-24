3ece5200eae9d65a41fc6cf87fe2e678f92e2949
Татьяна <tanyushat4tjana@yandex.ru>
2021-05-24 10:54:55 +0300

    Скомпилирована сборка приложения (папка build)

--- myapp/src/components/Main/Card.js
+++ myapp/src/components/Main/Card.js
@@ -6,9 +6,10 @@ function Card(props) {
   return (
     <div
       className="card col-12 col-md-6 col-xl-4"
-      data-category={props.item.category}
+      data-category={props.item.catId}
     >
       <img className="card-image" src={props.item.image} alt={props.item.alt} />
+      <span className="card-category">{props.item.catName}</span>
       <h3 className="card-title">{props.item.title}</h3>
       <p className="card-desc">{props.item.desc}</p>
       <a
--- myapp/src/components/Main/Catalog.js
+++ myapp/src/components/Main/Catalog.js
@@ -12,6 +12,15 @@ function Catalog() {
       <div className="container">
         <div className="row">
           <h2 className="col-12 section-title">Популярные интернет-магазины</h2>
+          <div className="category col-4">
+            <p>Выберите категорию: </p>
+            <select id="categoryList" className="category-list">
+              <option value="all">Все магазины</option>
+              <option value="marketplace">Маркетплейс</option>
+              <option value="tech">ИМ техники</option>
+              <option value="books">ИМ книг</option>
+            </select>
+          </div>
           <div className="cards col-12">{cardItems}</div>
         </div>
       </div>
